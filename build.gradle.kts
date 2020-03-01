aa95937e84ac21fec39f0bf9d4d68b1205fe0977
12242284@qq.com <Ttedit123!@#>
2020-03-01 12:39:56 +0800

    gradle config files

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    compile ("org.slf4j","slf4j-api","2.0.0-alpha1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
+    testCompile("ch.qos.logback", "logback-classic", "1.3.0-alpha5")
+    testCompile("io.kotlintest", "kotlintest-runner-junit5", "3.4.2")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
+    }
+}
